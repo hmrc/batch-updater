ffd2fc8cfe07a6ba608c8355fa076a1e8a438656
Jianfeng Tian <jianfeng.tian@digital.hmrc.gov.uk>
2015-12-14 10:41:24 +0000

    DC-169 upgrade play-auditing dependency

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -30,7 +30,7 @@ object HmrcBuild extends Build {
 private object BuildDependencies {
 
   object Compile {
-    val playAuditing = "uk.gov.hmrc" %% "play-auditing" % "1.5.0"
+    val playAuditing = "uk.gov.hmrc" %% "play-auditing" % "1.5.1"
   }
 
   sealed abstract class Test(scope: String) {
