92b3a268702ed0a138a9f232bf03e1d9c1b51397
Jakob Grunig <jakob.grunig@digital.hmrc.gov.uk>
2016-12-07 11:11:38 +0000

    DC-684 - Updated dependencies to play 2.5

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -8,45 +8,21 @@ import uk.gov.hmrc.versioning.SbtGitVersioning
 
 object HmrcBuild extends Build {
 
-  import BuildDependencies._
-  import uk.gov.hmrc.DefaultBuildSettings._
-
   val appName = "batch-updater"
 
   lazy val `batch-updater` = (project in file("."))
     .enablePlugins(SbtAutoBuildPlugin, SbtGitVersioning)
     .settings(
       name := appName,
-      libraryDependencies ++= Seq(
-        Compile.playAuditing,
-        Test.scalaTest,
-        Test.pegdown,
-        Test.scalaMock
-      ),
-      Developers()
+      libraryDependencies ++= appDependencies
     )
-}
 
-private object BuildDependencies {
-
-  object Compile {
-    val playAuditing = "uk.gov.hmrc" %% "play-auditing" % "1.9.0"
-  }
-
-  sealed abstract class Test(scope: String) {
-    val scalaTest = "org.scalatest" %% "scalatest" % "2.2.4" % scope
-    val pegdown = "org.pegdown" % "pegdown" % "1.5.0" % scope
-    val scalaMock = "org.scalamock" %% "scalamock-scalatest-support" % "3.2" % scope
-  }
-
-  object Test extends Test("test")
-
-}
-
-object Developers {
-
-  def apply() = developers := List(
-    Developer("howyp", "Howard Perrin", "howard.perrin@digital.hmrc.gov.uk", new URL("http://www.zuhlke.co.uk/")),
-    Developer("itsonlysoftware", "David Collins", "david.collins@digital.hmrc.gov.uk", new URL("http://www.equalexperts.com/"))
+  val appDependencies = Seq(
+    "uk.gov.hmrc"     %% "play-auditing"               % "2.4.0",
+    "org.scalatest"   %% "scalatest"                   % "2.2.4"   % "test",
+    "org.pegdown"     %  "pegdown"                     % "1.5.0"   % "test",
+    "org.scalamock"   %% "scalamock-scalatest-support" % "3.2"     % "test"
   )
+
 }
+
